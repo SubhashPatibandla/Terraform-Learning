2ff27059ee429c302f092fc1372698f2d770ffd9
subhash <subhashpatibandla85@gmail.com>
2025-10-05 09:36:01 +0530

    Day 2

--- Day 1/Demo/main.tf
+++ Day 1/Demo/main.tf
@@ -2,8 +2,14 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+ resource "aws_instance" "example" {
+   ami           = "ami-0360c520857e3138f"  # Specify an appropriate AMI ID
+   instance_type = "t2.micro"
+   count = 2
+ }
+
+ resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-bucket-12345-subhash"
 }
+
 
---
+++ Day 2/Readme.md
@@ -0,0 +1,9 @@
+## Terraform -- providers, variable, output, main, terraform.tfvars
+
+terraform-project/
+├── versions.tf      # required_providers + terraform version
+├── provider.tf      # provider "aws" configuration
+├── main.tf          # resources
+├── variables.tf     # input variables
+├── outputs.tf       # outputs
+├── terraform.tfvars  # actual values for variables
